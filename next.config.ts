0b9937b0f8afc0e98aaf92a80e9b353aa12242c9
Kavuma Julius <kavumajulius@github.com>
2026-08-25 11:51:36 +0300

    great

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,5 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  async rewrites() {
-    return [
-      {
-        source: "/.well-known/apple-developer-merchantid-domain-association",
-        destination: "https://whop.com/.well-known/apple-platform-integrator/apple-developer-merchantid-domain-association",
-      },
-    ];
-  },
-};
+const nextConfig: NextConfig = {};
 
 export default nextConfig;
